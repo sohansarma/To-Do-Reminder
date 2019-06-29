7271f0d1027704d187743291641b7fc2d2b69e22
Sohansarma <sohan@troopr.io>
2019-06-29 21:48:47 +0530

    Updates

---
+++ precache-manifest.829481bca327c1516f132afb743581d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e861a56c68b46a212584e8a4b04585",
+    "url": "/ToDoReminder/index.html"
+  },
+  {
+    "revision": "7139eeaddf4e29775f33",
+    "url": "/ToDoReminder/static/css/2.c42630f0.chunk.css"
+  },
+  {
+    "revision": "9ae5081cc69d028c3525",
+    "url": "/ToDoReminder/static/css/main.4fec9533.chunk.css"
+  },
+  {
+    "revision": "7139eeaddf4e29775f33",
+    "url": "/ToDoReminder/static/js/2.3386cde0.chunk.js"
+  },
+  {
+    "revision": "9ae5081cc69d028c3525",
+    "url": "/ToDoReminder/static/js/main.2e93df18.chunk.js"
+  },
+  {
+    "revision": "5fde1876127d6614c3c8",
+    "url": "/ToDoReminder/static/js/runtime~main.2fbd5fdd.js"
+  }
+]);
